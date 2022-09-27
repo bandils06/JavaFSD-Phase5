d3839b795016f71acbd7d9976fd2c8fdec4f9b33
Bandils06 <hrithikbandil@gmail.com>
2022-09-27 17:10:49 +0530

    .getTitle() method, .getCurrentUrl()

---
+++ Session1/src/GoogleTitleCheck.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.assertEquals;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleTitleCheck {
+
+	public static void main(String[] args) {
+		//Step 1: Declare a path
+		String path = "E:\\phase5-lib\\chromedriver_win32\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", path);
+		
+		//Step 2: Initialize the webdriver
+		// Requires selenium server version 3.5.0 (jar & server zip files)
+		
+		WebDriver driver = new ChromeDriver();
+		
+		//Step3: give the base url
+		
+		String url = "http://www.google.com";
+		driver.get(url);
+		
+		System.out.println("Title: "+driver.getTitle());
+//		System.out.println(driver.getCurrentUrl());
+		
+//		assertEquals("Google1", driver.getTitle());
+		driver.close();
+		
+		System.out.println("Driver working properly...");
+	}
+}
